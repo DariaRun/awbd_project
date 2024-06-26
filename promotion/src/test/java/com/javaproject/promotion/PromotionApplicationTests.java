aefd0c5621424ad40ef171fa658833170a8a3013
DariaRun <79482021+DariaRun@users.noreply.github.com>
2024-06-26 17:46:23 +0300

    Create PromotionApplicationTests.java

---
+++ promotion/src/test/java/com/javaproject/promotion/PromotionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaproject.promotion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
