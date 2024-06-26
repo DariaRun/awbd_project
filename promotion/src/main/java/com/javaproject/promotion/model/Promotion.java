5b7e7237c6fcec6d3963457418cedd745db07df8
DariaRun <79482021+DariaRun@users.noreply.github.com>
2024-06-26 17:43:35 +0300

    Create Promotion.java

---
+++ promotion/src/main/java/com/javaproject/promotion/model/Promotion.java
@@ -0,0 +1,14 @@
+package com.javaproject.promotion.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class Promotion {
+    private int week;
+    private int weekend;
+    private String versionId;
+}
